92db11d2ce71dbe4160d63e714b11207583b69e1
Sachintha Udara <sachintha.udara@axiatadigitallabs.com>
2022-07-26 23:14:44 +0530

    Initial Commit

---
+++ FinancialCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinancialCalc
+//
+//  Created by user on 2022-07-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
